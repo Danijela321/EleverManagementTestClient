2dd30e795176f70bfa28822b1620477b0b627610
Danijela <56878144+Danijela321@users.noreply.github.com>
2020-05-11 20:03:37 +0200

    ta bort interface och klas med konstruktor

--- src/Main.java
+++ src/Main.java
@@ -8,29 +8,31 @@ import javax.naming.NamingException;
 import com.yrgo.domain.Elever;
 import com.yrgo.elevermanagement.EleverManagementService;
 
-
+/**
+ * 
+ * @author Danijela
+ *
+ */
 
 public class Main {
 
 	public static void main(String[] args) throws NamingException {
 
-Properties jndiProperties = new Properties();
+		Properties jndiProperties = new Properties();
 
-jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY,
-                  "org.jboss.naming.remote.client.InitialContextFactory");
-jndiProperties.put(Context.PROVIDER_URL,"http-remoting://localhost:8080");
-jndiProperties.put("jboss.naming.client.ejb.context",true);
+		jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
+		jndiProperties.put(Context.PROVIDER_URL, "http-remoting://localhost:8080");
+		jndiProperties.put("jboss.naming.client.ejb.context", true);
 
-Context jndi = new InitialContext(jndiProperties);
-EleverManagementService service = (EleverManagementService) jndi.lookup("EleverManagementServerApplication/EleverManagementImplementation!com.yrgo.elevermanagement.EleverManagementService");
-	
-List<Elever> elever = service.getAllElever();
-for (Elever eleverna: elever) {
-	System.out.println(eleverna);
-}
-	
-	
+		Context jndi = new InitialContext(jndiProperties);
+		EleverManagementService service = (EleverManagementService) jndi.lookup(
+				"EleverManagementServerApplication/EleverManagementImplementation!com.yrgo.elevermanagement.EleverManagementService");
+
+		List<Elever> elever = service.getAllElever();
+		for (Elever eleverna : elever) {
+			System.out.println(eleverna);
+		}
+
 	}
-	
 
 }
