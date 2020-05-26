d1832e9ae02102d4e8c2f62e62d8a44fd18ac909
Danijela <56878144+Danijela321@users.noreply.github.com>
2020-05-26 19:50:17 +0200

    testing

--- src/Main.java
+++ src/Main.java
@@ -32,10 +32,10 @@ public class Main {
 		
 		EleverManagementService service = (EleverManagementService) jndi.lookup("EleverManagementServerApplication/EleverManagementImplementation!com.yrgo.elevermanagement.EleverManagementService");
 
-		Elever el2 = new Elever("Michael", "Henriksson", "Yrgo", 1);
-		Elever el3 = new Elever("Mate", "Lundqvist", "ITH", 2);
-		Elever el4 = new Elever("Mike", "Pedersson", "ITH", 1);
-		Elever el5 = new Elever("Mark", "Lundqvist", "Yrgo", 2);
+		Elever el2 = new Elever("Michael15", "Henriksson", "Yrgo", 1);
+		Elever el3 = new Elever("Mate15", "Lundqvist", "ITH", 2);
+		Elever el4 = new Elever("Mike15", "Pedersson", "ITH", 1);
+		Elever el5 = new Elever("Mark15", "Lundqvist", "Yrgo", 2);
 
 		try {
 			service.registerElever(el2);
@@ -53,6 +53,7 @@ public class Main {
 		}
 
 		System.out.println("\n\n");
+		System.out.println("I databas finns dessa elever med efternamn Lundqvist:\n");
 
 		List<Elever> eleverSurname = service.searchBysurname("Lundqvist");
 		for (Elever eleverna : eleverSurname) {
