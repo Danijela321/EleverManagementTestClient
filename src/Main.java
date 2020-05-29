8376809264ee2ff298c68fa0df4f5f49d078f62b
Danijela <56878144+Danijela321@users.noreply.github.com>
2020-05-29 21:54:15 +0200

    testing

--- src/Main.java
+++ src/Main.java
@@ -8,19 +8,13 @@ import javax.naming.NamingException;
 import com.yrgo.domain.Elever;
 import com.yrgo.elevermanagement.EleverManagementService;
 
-
-
-
-
-
-
 /**
  * @author danijela
  */
 
 public class Main {
 
-	public static void main(String[] args) throws NamingException{
+	public static void main(String[] args) throws NamingException {
 
 		Properties jndiProperties = new Properties();
 
@@ -29,38 +23,35 @@ public class Main {
 		jndiProperties.put("jboss.naming.client.ejb.context", true);
 
 		Context jndi = new InitialContext(jndiProperties);
-		
-		EleverManagementService service = (EleverManagementService) jndi.lookup("EleverManagementServerApplication/EleverManagementImplementation!com.yrgo.elevermanagement.EleverManagementService");
 
-		Elever el2 = new Elever("Michael20", "Henriksson", "Yrgo", 1);
-		Elever el3 = new Elever("Mate20", "Lundqvist", "ITH", 2);
-		Elever el4 = new Elever("Mike20", "Pedersson", "ITH", 1);
-		Elever el5 = new Elever("Mark20", "Lundqvist", "Yrgo", 2);
+		EleverManagementService service = (EleverManagementService) jndi.lookup(
+				"EleverManagementServerApplication/EleverManagementImplementation!com.yrgo.elevermanagement.EleverManagementService");
+
+		Elever el2 = new Elever("Sara", "Henriksson", "Yrgo", 1);
+//		Elever el3 = new Elever("Mate20", "Lundqvist", "ITH", 2);
+//		Elever el4 = new Elever("Mike20", "Pedersson", "ITH", 1);
+//		Elever el5 = new Elever("Mark20", "Lundqvist", "Yrgo", 2);
 
 		try {
 			service.registerElever(el2);
-			service.registerElever(el3);
-			service.registerElever(el4);
-			service.registerElever(el5);
+//			service.registerElever(el3);
+//			service.registerElever(el4);
+//			service.registerElever(el5);
+			System.out.println("Kör programmet flera gånger för att få checked exception");
 
 		} catch (Exception e) {
-			System.out.println("Something went wrong");
+			// beror på Math.random() funktion i EleverManegerServerApplication
+			// kör programmet till fel inte händer
+			System.out.println("Fel händer!");
+			System.out.println("Kör programmet flera gånger till checked exception inte händer");
+
 		}
 
 		List<Elever> elever = service.getAllElever();
 		for (Elever eleverna : elever) {
 			System.out.println(eleverna);
 		}
-		//aktivera när använder  production version
-	/*System.out.println("\n\n");
-		System.out.println("I databas finns dessa elever med efternamn Lundqvist:\n");
-
-		List<Elever> eleverSurname = service.searchBysurname("Lundqvist");
-		for (Elever eleverna : eleverSurname) {
-			System.out.println(eleverna);
-		}
-		*/
-
+	
 	}
 
 }
